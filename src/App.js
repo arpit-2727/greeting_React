d4aca23fada120084a404b1f30bbca2c9359aa5f
arpit-2727 <arpit1111k@gmail.com>
2023-12-20 13:33:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  let newDate = new Date();
+  newDate = newDate.getHours();
+  let greeting = "";
+  const styleCss = {};
+  if (newDate >= 1 && newDate < 12) {
+    greeting = "Good Morning";
+    styleCss.color = "green";
+  } else if (newDate >= 12 && newDate < 19) {
+    greeting = "Good Afternoon";
+    styleCss.color = "orange";
+  } else {
+    greeting = "Good Night";
+    styleCss.color = "black";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>
+          Hello , <span style={styleCss}> {greeting}</span>
+        </h1>
+      </div>
+    </>
   );
 }
-
 export default App;
